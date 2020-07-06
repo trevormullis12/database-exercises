bdaa8f570564bf1714a1fd1a925a762d246a42e8
trevormullis12 <trevor.mullis12@gmail.com>
2020-07-06 14:55:02 -0500

    added basic files

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS codeup_test_db;
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums(
+     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+     artist VARCHAR(100),
+     name  VARCHAR(100),
+     release_date INT UNSIGNED,
+     PRIMARY KEY (id)
+);
