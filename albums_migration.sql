59e4b9994ccb3cbacc94d8530de67fdf58c3278a
trevormullis12 <trevor.mullis12@gmail.com>
2020-07-06 15:02:12 -0500

    made albums table

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,8 +6,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     artist VARCHAR(100),
-     name  VARCHAR(100),
+     artist VARCHAR(50),
+     name  VARCHAR(50),
      release_date INT UNSIGNED,
+     sales DECIMAL(13, 2),
+     genre VARCHAR(50),
      PRIMARY KEY (id)
 );
