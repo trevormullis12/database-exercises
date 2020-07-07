ccad18d7f37643ff61d4256dcb37508447911b46
trevormullis12 <trevor.mullis12@gmail.com>
2020-07-07 13:36:08 -0500

    added album seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -6,10 +6,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE IF NOT EXISTS albums(
      id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-     artist VARCHAR(50),
-     name  VARCHAR(50),
+     artist VARCHAR(100),
+     name  VARCHAR(100),
      release_date INT UNSIGNED,
-     sales DECIMAL(13, 2),
-     genre VARCHAR(50),
+     sales DECIMAL(4, 2),
+     genre VARCHAR(100),
      PRIMARY KEY (id)
 );
